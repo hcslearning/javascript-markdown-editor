847c01153b1805f9db3a62049ecb2cb786fc9e65
HCS Learning <learning@hardcybersoft.cl>
2021-04-03 04:34:00 -0300

    ejemplo con CKEditor usando Showdown para convertir de HTML a Markdown, ya que no supimos hacer funcionar el plugin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import { useState } from 'react';
+//import Markdown from '@ckeditor/ckeditor5-markdown-gfm'
+import showdown from 'showdown'
 
 function App() {
   const [texto, setTexto] = useState('')
 
   return (
     <div className="App">
-      <CKEditor editor={ClassicEditor} onChange={(event, editor) => setTexto(editor.getData())} />
+      <CKEditor editor={ClassicEditor} onChange={(event, editor) => setTexto( (new showdown.Converter()).makeMarkdown( editor.getData() ) )} />
 
       texto<br />
       -------<br />
